83a5ac283e2d757ff6594a63f9a559dcc107df2c
tun43p <git@tun43p.com>
2022-09-18 16:21:30 +0200

    chore: clean fish config file

--- fish/config/public/aliases.fish
+++ fish/config/public/aliases.fish
@@ -6,5 +6,3 @@ alias code codium
 
 alias dots "cd $DOTFILES"
 alias repos "cd $REPOSITORIES"
-
-alias up="sudo killall snap-store && sudo apt-get update && sudo apt-get full-upgrade && sudo snap refresh"
