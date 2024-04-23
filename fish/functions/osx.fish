0d458ace1e2ff85256f144a7c538f17d3e074294
Alex <git@tun43p.com>
2024-04-23 18:21:56 +0200

    feat: create an osx function to do some utils things

---
+++ fish/functions/osx.fish
@@ -0,0 +1,23 @@
+function osx -d "Some utils for OSX"
+    set -l help "Usage: osx <string>"
+
+    if test (count $argv) -eq 0
+        echo $help
+        return 1
+    end
+
+    switch $argv[1]
+        case help h
+            echo $help
+        case ip
+            ipconfig getifaddr en0
+        case hide
+            chflags hidden $argv[2]
+        case unhide
+            chflags nohidden $argv[2]
+        case '*'
+            echo "Unknown script"
+            return 1
+    end
+
+end
