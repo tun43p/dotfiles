ac739e9471c3a2eba1396f2d50a07b0c430258ef
Alex <git@tun43p.com>
2024-05-03 13:49:28 +0200

    feat: create a script to init a new golang project

---
+++ fish/functions/go-init.fish
@@ -0,0 +1,33 @@
+# TODO(tun43p): Explain the purpose of this script
+
+function go-init -d "Create a new Golang project"
+    set -l help "Usage: go-init <project-name>"
+
+    # TODO(tun43p): Add a flag to specify the project path
+    set -l tag github.com/tun43p
+    set -l name $argv[1]
+    set -l dir $CODE/$name
+    set -l src $GOPATH/src
+
+    if test (count $argv) -eq 0
+        echo $help
+        return 1
+    end
+
+    if test -d $dir
+        echo "Project already exists"
+        return 1
+    end
+
+    mkdir $dir && cd $dir
+    go mod init $name
+
+    if not test -d $src/$tag
+        mkdir -p $src/$tag
+        echo "Directory created at $src/$tag"
+    end
+
+    ln -sf $dir $src/$tag/$name
+
+    echo "Project created at $dir"
+end
