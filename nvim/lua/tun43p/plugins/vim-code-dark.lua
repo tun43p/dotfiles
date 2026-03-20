ec7719901834ddb0857afcba8abab5f4936ac2b9
tun43p <tun43p@icloud.com>
2026-03-20 22:46:04 +0100

    feat: remove tokyo night and use vscode theme

--- nvim/lua/tun43p/plugins/vim-code-dark.lua
+++ nvim/lua/tun43p/plugins/vim-code-dark.lua
@@ -1,38 +1,13 @@
--- NOTE: vim-code-dark replaced by tokyonight
--- return {
--- 	"tomasiser/vim-code-dark",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		vim.cmd("set t_Co=256")
--- 		vim.cmd("set t_ut=")
--- 		vim.cmd("let g:codedark_modern=1")
--- 		vim.cmd("let g:codedark_transparent=1")
--- 		vim.cmd.colorscheme("codedark")
--- 	end,
--- }
-
 return {
-	"folke/tokyonight.nvim",
+	"Mofiqul/vscode.nvim",
 	lazy = false,
 	priority = 1000,
 	opts = {
-		style = "night",
 		transparent = true,
-		terminal_colors = true,
-		styles = {
-			comments = { italic = true },
-			keywords = { italic = true },
-			sidebars = "transparent",
-			floats = "transparent",
-		},
-		on_highlights = function(hl, c)
-			-- Better visibility for snacks picker
-			hl.SnacksPickerMatch = { fg = c.blue, bold = true }
-		end,
+		italic_comments = true,
 	},
 	config = function(_, opts)
-		require("tokyonight").setup(opts)
-		vim.cmd.colorscheme("tokyonight")
+		require("vscode").setup(opts)
+		vim.cmd.colorscheme("vscode")
 	end,
 }
