362146e814499278f421978a9e6295b21703de92
tun43p <tun43p@icloud.com>
2026-05-13 20:01:50 +0200

    feat: switch theme for oxocarbon

--- nvim/lua/tun43p/plugins/lualine.lua
+++ nvim/lua/tun43p/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "nord",
+				theme = "auto",
 				icons_enabled = true,
 				component_separators = { left = "", right = "" },
 				section_separators = { left = "", right = "" },
--- nvim/lua/tun43p/plugins/oxocarbon.lua
+++ nvim/lua/tun43p/plugins/oxocarbon.lua
@@ -1,17 +1,11 @@
 return {
-  "arcticicestudio/nord-vim",
+  "nyoom-engineering/oxocarbon.nvim",
   lazy = false,
   priority = 1000,
   config = function()
-    vim.g.nord_italic = 1
-    vim.g.nord_italic_comments = 1
-    vim.g.nord_underline = 1
-    vim.g.nord_uniform_diff_background = 1
-    vim.g.nord_cursor_line_number_background = 1
+    vim.opt.background = "dark"
+    vim.cmd.colorscheme("oxocarbon")
 
-    vim.cmd.colorscheme("nord")
-
-    -- Background transparent (nord-vim n'a pas d'option native)
     local transparent_groups = {
       "Normal", "NormalNC", "NormalFloat", "FloatBorder",
       "SignColumn", "EndOfBuffer", "LineNr", "CursorLineNr",
