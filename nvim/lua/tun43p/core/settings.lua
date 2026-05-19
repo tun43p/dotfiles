f3617f5f6c9a32711b36797bb365765871fe9bfa
tun43p <tun43p@icloud.com>
2026-05-19 09:05:37 +0200

    fix: change cwd to the argument passed to nvim (e.g. nvim $DOTS)

--- nvim/lua/tun43p/core/settings.lua
+++ nvim/lua/tun43p/core/settings.lua
@@ -90,6 +90,16 @@ vim.api.nvim_create_autocmd({ "BufEnter", "TermOpen" }, {
 	end,
 })
 
+-- change cwd to the argument passed to nvim (e.g. `nvim $DOTS`)
+vim.api.nvim_create_autocmd("VimEnter", {
+	callback = function()
+		local arg = vim.fn.argv(0)
+		if arg ~= "" and vim.fn.isdirectory(arg) == 1 then
+			vim.cmd("cd " .. vim.fn.fnameescape(arg))
+		end
+	end,
+})
+
 -- colorcolumn at 80 for files
 vim.api.nvim_create_autocmd("FileType", {
 	pattern = { "markdown", "javascript", "javascriptreact", "typescript", "typescriptreact" },
