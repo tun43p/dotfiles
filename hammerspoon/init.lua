3b1ec53c38c110f5de83bf1103b49f2a5c35271e
tun43p <tun43p@icloud.com>
2026-05-27 21:17:40 +0200

    feat: update hammerspoon config

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,4 +1,5 @@
-hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "g", function()
+-- Launch Alacritty
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "a", function()
 	local app = hs.application.get("Alacritty")
 
 	if not app then
@@ -15,6 +16,12 @@ hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "g", function()
 	end)
 end)
 
+-- Launch Visual Studio Code
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "c", function()
+	hs.execute("open -a 'Visual Studio Code'")
+end)
+
+-- Launch Neovim
 hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "n", function()
 	local app = hs.application.get("Alacritty")
 
@@ -40,6 +47,33 @@ hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "n", function()
 	end)
 end)
 
+-- Launch Wiki
+hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "w", function()
+	local app = hs.application.get("Alacritty")
+
+	if not app then
+		hs.application.launchOrFocus("Alacritty")
+		hs.timer.doAfter(0.1, function()
+			hs.eventtap.keyStroke({ "cmd" }, "n")
+			hs.timer.doAfter(0.1, function()
+				hs.eventtap.keyStrokes("nvim ~/.wiki/index.md")
+				hs.eventtap.keyStroke({}, "return")
+			end)
+		end)
+		return
+	end
+
+	app:activate()
+	hs.timer.doAfter(0.1, function()
+		hs.eventtap.keyStroke({ "cmd" }, "n")
+		hs.timer.doAfter(0.1, function()
+			hs.eventtap.keyStrokes("nvim ~/.wiki/index.md")
+			hs.eventtap.keyStroke({}, "return")
+		end)
+	end)
+end)
+
+-- Launch Zen
 hs.hotkey.bind({ "ctrl", "alt", "cmd" }, "z", function()
 	hs.execute("open -a Zen")
 end)
